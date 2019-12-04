2850f888078a2a44d74bce6117f2297e3a13b9a8
shan <amimulehshan29@gmail.com>
2019-12-05 00:16:57 +0600

    db final

--- rtms.sql
+++ rtms.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.5.4.1deb2ubuntu2.1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 29, 2016 at 02:24 PM
--- Server version: 5.7.9
--- PHP Version: 5.6.16
+-- Host: localhost
+-- Generation Time: Dec 04, 2019 at 06:16 PM
+-- Server version: 5.7.28-0ubuntu0.16.04.2
+-- PHP Version: 7.0.33-0ubuntu0.16.04.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,8 +26,8 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -36,8 +36,8 @@ CREATE TABLE IF NOT EXISTS `admin` (
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`username`, `password`) VALUES
-('admin', '4042');
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', '1234');
 
 -- --------------------------------------------------------
 
@@ -45,8 +45,7 @@ INSERT INTO `admin` (`username`, `password`) VALUES
 -- Table structure for table `dashboard`
 --
 
-DROP TABLE IF EXISTS `dashboard`;
-CREATE TABLE IF NOT EXISTS `dashboard` (
+CREATE TABLE `dashboard` (
   `username` text NOT NULL,
   `train` text NOT NULL,
   `sourse` text NOT NULL,
@@ -56,20 +55,8 @@ CREATE TABLE IF NOT EXISTS `dashboard` (
   `cost` text NOT NULL,
   `bank` text NOT NULL,
   `ac` text NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `dashboard`
---
-
-INSERT INTO `dashboard` (`username`, `train`, `sourse`, `destination`, `date`, `sit`, `cost`, `bank`, `ac`, `id`) VALUES
-('salehin6', 'Dhumketu Express', 'Dhaka', 'Rajshahi', '1234', 'VIP 2', '1600', 'DBBL', 'acn', 2),
-('Dider', 'Dhumketu Express', 'Dhaka', 'Rajshahi', '1.09.14-9am Dhaka-Rajshahi and 9pm Rajshahi to Dhaka ', 'Normal 1', '401', 'DBBL', 'acn', 3),
-('Dider', 'Dhumketu Express', 'Dhaka', 'Rajshahi', '20.2.2016-9am Dhaka-Rajshahi and 9pm Rajshahi to Dhaka ', 'Normal 1', '401', 'DBBL', '4041', 4),
-('Gayle', 'Sundorban Express', 'Dhaka', 'Khulna', '10.08.2016-7.00pm khulna to Dhaka 7.00am Dhaka to khulna', 'VIP 3', '2997', 'PayPal', '3131242423142341', 5),
-('salehin6', 'Sundorban Express', 'Dhaka', 'Khulna', '1/2/2016-7.00pm khulna to Dhaka 7.00am Dhaka to khulna', 'VIP 1', '999', 'DBBL', '420420', 6);
+  `id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -77,13 +64,11 @@ INSERT INTO `dashboard` (`username`, `train`, `sourse`, `destination`, `date`, `
 -- Table structure for table `station`
 --
 
-DROP TABLE IF EXISTS `station`;
-CREATE TABLE IF NOT EXISTS `station` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `station` (
+  `id` int(11) NOT NULL,
   `name` text NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+  `description` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `station`
@@ -102,18 +87,16 @@ INSERT INTO `station` (`id`, `name`, `description`) VALUES
 -- Table structure for table `train`
 --
 
-DROP TABLE IF EXISTS `train`;
-CREATE TABLE IF NOT EXISTS `train` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `train` (
+  `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `place1` text NOT NULL,
   `place2` text NOT NULL,
   `route` text NOT NULL,
   `time` text NOT NULL,
   `vip` int(11) NOT NULL,
-  `normal` int(11) NOT NULL,
-  PRIMARY KEY (`id`,`name`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+  `normal` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `train`
@@ -131,8 +114,7 @@ INSERT INTO `train` (`id`, `name`, `place1`, `place2`, `route`, `time`, `vip`, `
 -- Table structure for table `user_info`
 --
 
-DROP TABLE IF EXISTS `user_info`;
-CREATE TABLE IF NOT EXISTS `user_info` (
+CREATE TABLE `user_info` (
   `fullname` text NOT NULL,
   `username` varchar(20) NOT NULL,
   `address` text NOT NULL,
@@ -140,8 +122,7 @@ CREATE TABLE IF NOT EXISTS `user_info` (
   `bankname` text NOT NULL,
   `acn` text NOT NULL,
   `password` text NOT NULL,
-  `email` text NOT NULL,
-  PRIMARY KEY (`username`)
+  `email` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -149,13 +130,66 @@ CREATE TABLE IF NOT EXISTS `user_info` (
 --
 
 INSERT INTO `user_info` (`fullname`, `username`, `address`, `mobile`, `bankname`, `acn`, `password`, `email`) VALUES
-('shoaeb salehin', 'salehin6', 'dhaka', '01680237961', 'DBBL', '420420', '022123', 'shoaeb4042@gmail.com'),
-('Dider', 'Dider', 'Dhaka', '01710454545', 'DBBL', '4041', 'ddr.srn@gmail.com', 'ddr.srn@gmail.com'),
-('Nayem Miah', 'miah', 'Ghana', ' 024 222 3344', 'SCB', '3131242423142343', 'miah', 'nayem3469@gmail.com'),
-('Chis Gayle', 'Gayle', 'WI', '8769272779', 'PayPal', '3131242423142341', '1234', 'gayle@yahoo.com'),
-('Sadia Enterprise', 'Sadia', 'Uganda', '25614329847', 'City Bank', '313124242314', '1234', 'nayeem3469@gmail.com'),
-('S. salehin', 'salehin', 'Keniya', '78123414543', 'AB', '3131242423142', '1234', 'shoaeb.code71@gmail.com');
+('Amimul Ehshan', 'amims71', 'dhaka', '01752804620', 'DBBL', '123123', '123', 'amims71@gmail.com');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dashboard`
+--
+ALTER TABLE `dashboard`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `station`
+--
+ALTER TABLE `station`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `train`
+--
+ALTER TABLE `train`
+  ADD PRIMARY KEY (`id`,`name`);
+
+--
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `dashboard`
+--
+ALTER TABLE `dashboard`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `station`
+--
+ALTER TABLE `station`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `train`
+--
+ALTER TABLE `train`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
